f07e8ad1496743fae8309a3ed7b4e000b0db3c5c
olkornieva <olyashines@gmail.com>
2016-11-24 14:10:51 +0200

    Update Navigation.java

--- src/main/java/rozetka_UI/Navigation.java
+++ src/main/java/rozetka_UI/Navigation.java
@@ -9,10 +9,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Navigation {
 	
-	   System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
-	    public static WebDriver driver = new FirefoxDriver();
+	     public  static WebDriver driver;
+	
+	    public static void visitMainPage()  throws Exception {
+	    System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+	    driver = new FirefoxDriver();
 
-	    public static void visitMainPage() {
 	        driver.get("http://www.rozetka.com.ua");
 	        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
 	            public Boolean apply(WebDriver d) {
